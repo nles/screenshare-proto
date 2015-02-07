9ab9aff4e928c6830345b9eaf9c625a8ff72c480
Nikke <nileskel@gmail.com>
2015-02-07 20:41:26 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Proto::Application.config.secret_key_base = '21f54befbc1697b4aa13005898db11a66ad5a6eca7b629f06f8959a1f1180bf16945ff4cbb85993a2039e167407ea5b0ba01a5b2d890114fe48da5f035ce6864'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Proto::Application.routes.draw do
+  root 'home#index'
+end
