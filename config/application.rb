70281fa45f39c7e340e3d13ef3f8c52857da6cbd
Tommi Tapaninaho <tommi@tapaninaho.com>
2015-02-26 11:27:05 +0200

    Added divider to top, no functionality yet. Imported font doesnt work

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,11 @@ module Proto
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    #Adding some fonts
+    config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
