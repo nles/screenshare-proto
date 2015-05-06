ead4cb13d7848e6b171a19b170e5cf0999124ae3
Nikke <nileskel@gmail.com>
2015-05-06 10:34:36 +0300

    portal demo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  def portal_demo
+    render 'portal_demo', :layout => 'portal_demo'
+  end
   def extension_just_installed
     @extension_just_installed = true
     render 'index'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Proto::Application.routes.draw do
   root 'home#index'
+  get '/demo' => 'home#portal_demo'
   get '/extension-just-installed' => 'home#extension_just_installed'
   get '/:screen_id' => 'home#view'
 end
